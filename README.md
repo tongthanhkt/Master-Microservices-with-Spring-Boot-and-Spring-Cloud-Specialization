e2db71751f3960180e000175c1810c236f094ab3
Thanh Tong <your@email.com>
2026-01-07 15:48:01 +0700

    feat(SPB): Course 1 Module 3 Step 05 - Enhancing the Hello World REST API with a Path Variable

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Master-Microservices-with-Spring-Boot-and-Spring-Cloud-Specialization
 feat(SPB): Course 1 Module 3 Step 1
 feat(SPB): Course 1 Module 3 Step 02 - Creating a Hello World REST API with Spring Boot
+feat(SPB): Course 1 Module 3 Step 03 - Enhancing the Hello World REST API to return a Bean
+feat(SPB): Course 1 Module 3 Step 04
+feat(SPB): Course 1 Module 3 Step 05: Step 05 - Enhancing the Hello World REST API with a Path Variable
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello world");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world, %s", name));
     }
 }
